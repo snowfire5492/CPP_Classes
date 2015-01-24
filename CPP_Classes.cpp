2bbcb1043da832bac32abdc545e2883fbaa4a977
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2015-01-24 15:12:12 -0800

    update

---
+++ CPP_Classes.cpp
@@ -0,0 +1,23 @@
+/*
+ *
+ *
+ *
+ * Classes in C++
+ *
+ *
+ */
+
+
+#include <iostream>
+#include "Cat.h"
+using namespace std;
+
+int main(){
+
+	Cat elsa;		// instantiated Cat object
+	elsa.speak();
+	elsa.jump();
+
+	return 0;
+}
+
