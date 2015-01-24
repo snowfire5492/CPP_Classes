beab47623f20b4b08033c349c95693163ce97000
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2015-01-24 15:12:12 -0800

    update

---
+++ Cat.cpp
@@ -0,0 +1,20 @@
+/*
+ * Cat.cpp
+ *
+ *  Created on: Apr 18, 2019
+ *      Author: snowf
+ */
+
+#include <iostream>
+#include "Cat.h"
+using namespace std;
+
+
+void Cat::speak(){
+	cout << "meoowwww!!!" << endl;
+}
+
+void Cat::jump(){
+	cout << "jumping..." << endl;
+}
+
