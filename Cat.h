6ad16c0b0c1de8465ec8087fbe773057f3059673
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2015-01-24 15:12:12 -0800

    update

---
+++ Cat.h
@@ -0,0 +1,20 @@
+/*
+ * Cat.h
+ *
+ *  Created on: Apr 18, 2019
+ *      Author: snowf
+ */
+
+#ifndef CAT_H_
+#define CAT_H_
+
+
+class Cat{
+public:
+	void speak();
+	void jump();
+
+};
+
+#endif /* CAT_H_ */
+
